d5eb0a6f3eb2efe0dea1599cf3c40cb5ff7f8eba
Cpt-Hook <ondra.stanicek@gmail.com>
2018-04-25 16:11:07 +0200

    Displaying average of all temp sensors.

--- Plugin/src/main/java/com/sonelli/juicessh/performancemonitor/controllers/TemperatureController.java
+++ Plugin/src/main/java/com/sonelli/juicessh/performancemonitor/controllers/TemperatureController.java
@@ -22,13 +22,25 @@ public class TemperatureController extends BaseController {
 
         final Handler handler = new Handler();
         handler.post(new Runnable() {
+
+            private int numOfLines;
+            private double sumOfTemps;
+
             @Override
             public void run() {
+                numOfLines = 0;
+                sumOfTemps = 0;
                 try {
                     getPluginClient().executeCommandOnSession(getSessionId(),getSessionKey(), "cat /sys/class/thermal/thermal_zone*/temp", new OnSessionExecuteListener() {
                         @Override
                         public void onCompleted(int exitCode) {
+                            Log.d(TAG,  "Exit code: " + exitCode);
                             switch(exitCode){
+                                case 0:
+                                    sumOfTemps/=numOfLines; //calc avarage
+                                    sumOfTemps = (double)Math.round(sumOfTemps*10)/10; //one decimal place
+                                    setText(Double.toString(sumOfTemps) + "°C");
+                                    break;
                                 case 127:
                                     setText(getString(R.string.error));
                                     Log.d(TAG, "Tried to run a command but the command was not found on the server");
@@ -38,11 +50,11 @@ public class TemperatureController extends BaseController {
 
                         @Override
                         public void onOutputLine(String line) {
+                            Log.d(TAG,  "Line: " + line);
                             //input is in millidegrees C
                             double temp = Double.parseDouble(line)/1000;
-                            //only one decimal place
-                            temp = (double)Math.round(temp*10)/10;
-                            setText(Double.toString(temp) + "°C");
+                            sumOfTemps+=temp;
+                            numOfLines++;
                         }
 
                         @Override
